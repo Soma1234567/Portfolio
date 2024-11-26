3b222f41d799d6f4e88c75ac677295ec25e87f3d
soma <somasekharboya9441@gmail.com>
2024-11-26 12:16:21 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
